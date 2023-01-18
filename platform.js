ccdb6015ae6f3264826a50de700fdba5fc164dfd
Lean-Bustos <leanbustos@gmail.com>
2023-01-18 18:47:12 -0300

    estructura html

---
+++ platform.js
@@ -0,0 +1,14 @@
+
+
+
+// 👉 Crear un archivo HTML que contenga un contenedor en el cual se renderice una card en la que deberán renderizar el nombre, imagen, ingredientes y precio de una pizza (Estilizarlo con CSS 🎨). Además, deberán renderizar el mismo input de tipo number y botón de la entrega anterior.
+
+// Deberemos colocar un numero en el input y, al apretar el botón, deberá renderizar en el contenedor una card con los datos de la pizza cuyo id coincida con el número ingresado en el input.
+
+// los errores no se guardan en el LS).
+
+// ¿Cuál es el desafío final?
+// Deberán guardar en localStorage la última pizza buscada y renderizada, y al recargar la página será esa pizza la que se deberá mostrar en la página.
+
+
+// 🆙 Entregar el link de Github , en el cual debe estar linkeado el deploy del Vercel de su aplicación (mediante Github nosotros deberíamos poder ver el Vercel vinculado a su repositorio). 
