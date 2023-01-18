2d20005790a29e9026360f26c6414e4796b40801
Lean-Bustos <leanbustos@gmail.com>
2023-01-18 20:42:06 -0300

    agregado de estilos

--- platform.js
+++ platform.js
@@ -1,4 +1,66 @@
 
+const nav = document.getElementById('navbar');
+const menu = document.getElementById('menu');
+const carContainer = document.getElementById('card-container')
+
+const objetoPizza = JSON.parse(localStorage.getItem("Producto"));
+
+if (objetoPizza){ 
+    const renderProducto =  
+                `<div class="card-container">
+                <h2 class="titulo-card">${objetoPizza.nombre}</h2>
+                <img class="img-card" src="${objetoPizza.img}" alt"img" >
+                <h3 class="precio-card">$${objetoPizza.precio}</h3>
+                <p class="p-card"> ${objetoPizza.ingredientes} </p>
+                </div>`
+
+    comanda.innerHTML = renderProducto;
+}
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    
+    const ingresoId = Number(input.value);
+   
+    const encargo = pizzas.filter(pizza => ingresoId === pizza.id);
+
+    if( input.value === ''){
+        const ingresarAlgo = 
+        `<div class="card-container">
+        <h2 class="error-ingreso">
+        Debes ingresar un valor
+        para hacer tu Pedido 
+        </h2>
+        </div>`
+        comanda.innerHTML = ingresarAlgo
+        localStorage.clear();
+
+    } else if(!encargo[0]){
+        const ingresoInvalido =
+         `<div class="card-container">
+        <h2 class="error-ingreso">
+        Los Sentimos!!
+        Ese Producto No esta en nuestra Lista
+         </h2>
+         </div>`
+        comanda.innerHTML = ingresoInvalido
+        localStorage.clear();
+
+    } else{        
+            const renderPizzaYPrecio =  
+            `<div class="card-container">
+            <h2 class="titulo-card">${encargo[0].nombre}</h2>
+            <img class="img-card" src="${encargo[0].img}" alt"img" >
+            <h3 class="precio-card">$${encargo[0].precio}</h3>
+            <p class="p-card"> ${encargo[0].ingredientes} </p>
+            </div>`
+        comanda.innerHTML = renderPizzaYPrecio;
+        }
+
+        form.reset();
+
+        localStorage.setItem("Producto", JSON.stringify(encargo[0]));
+})
 
 
 // 👉 Crear un archivo HTML que contenga un contenedor en el cual se renderice una card en la que deberán renderizar el nombre, imagen, ingredientes y precio de una pizza (Estilizarlo con CSS 🎨). Además, deberán renderizar el mismo input de tipo number y botón de la entrega anterior.
